f6fbbc515fbd56d92ae35bf1861cc76255f89f6c
ShimantaBhuyan <bhuyanshimanta@gmail.com>
2023-04-21 18:03:10 +0530

    feat: add config.hosts in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts = 'askgita.up.railway.app'
+  config.hosts << 'askgita.up.railway.app'
 end
